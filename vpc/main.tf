d4255fb0e87631fc2e0fe51c4101271a79eba71c
abhaygoyall <aby.goyal@gmail.com>
2026-08-09 12:26:44 +0530

    vpc-regional-nat

--- vpc/main.tf
+++ vpc/main.tf
@@ -73,3 +73,40 @@ resource "aws_route_table_association" "public_2b" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.regional.id
+  }
+  tags = {
+    Name = "terraform-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_2a" {
+  subnet_id      = aws_subnet.private_2a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_2b" {
+  subnet_id      = aws_subnet.private_2b.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_eip" "regional_nat" {
+  domain = "vpc"
+  tags = {
+    Name = "terraform-regional-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "regional" {
+  connectivity_type = "public"
+  availability_mode = "regional"
+  allocation_id     = aws_eip.regional_nat.id
+  vpc_id           = aws_vpc.main.id
+  tags = {
+    Name = "terraform-regional-nat-gateway"
+  }
+}
