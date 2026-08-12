39e2ffb7aa82918effdd2242abd59ed190f7bcc9
abhaygoyall <aby.goyal@gmail.com>
2026-08-12 14:13:17 +0530

    stage-sql-config

---
+++ environment/stage/datastore/mysql/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+
+  backend "s3" {
+    bucket = "terraform-state-by-abhay"
+    key    = "environment/stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+resource "aws_db_instance" "db" {
+  identifier_prefix   = "terraform-by-abhay"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t3.micro"
+  skip_final_snapshot = true
+  db_name             = var.db_name
+  username         = var.db_username
+  password         = var.db_password
+}
